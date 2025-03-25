db09089ac2c269c9dde50174535528c5fe40009a
Ngineer101 <nicolaasb@pm.me>
2025-03-25 16:28:59 +0100

    gen db types and add supabase browser client

---
+++ src/lib/supabase/browser.ts
@@ -0,0 +1,34 @@
+import { createBrowserClient } from '@supabase/ssr';
+import { COOKIE_STORAGE_KEY } from '@/lib/types/constants';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+export const supabaseBrowserClient = createBrowserClient(
+  supabaseUrl,
+  supabaseAnonKey,
+  {
+    auth: {
+      persistSession: true,
+      storageKey: COOKIE_STORAGE_KEY,
+      storage: {
+        getItem: (key) => {
+          if (typeof window !== 'undefined') {
+            return window.localStorage.getItem(key);
+          }
+          return null;
+        },
+        setItem: (key, value) => {
+          if (typeof window !== 'undefined') {
+            window.localStorage.setItem(key, value);
+          }
+        },
+        removeItem: (key) => {
+          if (typeof window !== 'undefined') {
+            window.localStorage.removeItem(key);
+          }
+        },
+      },
+    },
+  }
+);
